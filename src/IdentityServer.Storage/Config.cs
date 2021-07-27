21ba835aabbf495394f59b4ad676323caddfad8f
renzhsh <renzhsh@126.com>
2021-07-27 10:21:28 +0800

    搭建Id4存储服务--初始化

---
+++ src/IdentityServer.Storage/Config.cs
@@ -0,0 +1,23 @@
+﻿using IdentityServer4.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IdentityServer.Storage
+{
+    public class Config
+    {
+        /// <summary>
+        /// 定义API资源
+        /// </summary>
+        public static IEnumerable<ApiResource> ApiResources =>
+             new List<ApiResource>
+              {
+                    new ApiResource("api1", "我的第一个API")
+              };
+
+        public static IEnumerable<Client> Clients =>
+             new List<Client> { };
+    }
+}
---
+++ src/IdentityServer.Storage/Startup.cs
@@ -0,0 +1,61 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IdentityServer.Storage
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddIdentityServer()//注册服务
+                .AddDeveloperSigningCredential() // 添加临时秘钥
+                .AddInMemoryApiResources(Config.ApiResources)//配置API资源
+                .AddInMemoryClients(Config.Clients)//配置类定义的授权客户端
+                ;
+
+            services.AddControllersWithViews();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+            }
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseIdentityServer();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
